0ef38764fc59faecc1c58333ab9eecde543a8014
Alex Lementuev <a.lementuev@gmail.com>
2016-02-14 18:56:05 -0800

    Updated to v 0.2.0

--- Project/Assets/LunarConsole/Scripts/Constants.cs
+++ Project/Assets/LunarConsole/Scripts/Constants.cs
@@ -26,7 +26,7 @@ namespace LunarConsoleInternal
 {
     public static class Constants
     {
-        public static readonly string Version = "0.1.0b";
+        public static readonly string Version = "0.2.0b";
         public static readonly string UpdateJsonURL = "https://raw.githubusercontent.com/SpaceMadness/lunar-unity-console/master/Builder/updater.json";
 
         public static readonly string PluginName = "LunarConsole";
