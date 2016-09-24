769bf71a81160c8e7e84a854e594287e9cbe0ee1
Alex Lementuev <a.lementuev@gmail.com>
2016-09-23 23:49:42 -0700

    Added better logger UI

---
+++ Builder/TestProject/Assets/Logger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+
+public class Logger : MonoBehaviour
+{
+    public void LogDebug()
+    {
+        Debug.Log("Log debug");
+    }
+
+    public void LogWarning()
+    {
+        Debug.Log("Log warning");
+    }
+
+    public void LogError()
+    {
+        Debug.Log("Log error");
+    }
+
+    public void ThrowException()
+    {
+        throw new Exception("Error");
+    }
+}
