c68ce1c69440e4649093a9b43029a4d58fb6654a
Alex Lementuev <a.lementuev@gmail.com>
2017-02-06 18:48:24 -0800

    [IOS] Free version progress

---
+++ Native/iOS/LunarConsole/LunarConsole/Free/Console/Plugin/LUConsolePluginImp.m
@@ -0,0 +1,29 @@
+//
+//  LUConsolePluginImp.m
+//  LunarConsole
+//
+//  Created by Alex Lementuev on 2/6/17.
+//  Copyright © 2017 Space Madness. All rights reserved.
+//
+
+#import "LUConsolePluginImp.h"
+@implementation LUConsolePluginImp
+
+- (instancetype)initWithPlugin:(LUConsolePlugin *)plugin
+{
+    self = [super init];
+    if (self)
+    {
+    }
+    return self;
+}
+
+- (void)showOverlay
+{
+}
+
+- (void)hideOverlay
+{
+}
+
+@end
