572c6dafa777139b6a881c8c08d074dd000672f2
Alex Lementuev <a.lementuev@gmail.com>
2017-02-18 18:38:55 -0800

    [IOS] Added range variable type

---
+++ Native/iOS/LunarConsole/LunarConsole/Full/Controllers/Variables/LUCVarRangeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  LUCVarRangeTableViewCell.h
+//  LunarConsole
+//
+//  Created by Alex Lementuev on 2/18/17.
+//  Copyright © 2017 Space Madness. All rights reserved.
+//
+
+#import "LUCVarTableViewCell.h"
+
+@interface LUCVarRangeTableViewCell : LUCVarTableViewCell
+
+@end
