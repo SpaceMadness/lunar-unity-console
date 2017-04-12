9637007cf5161374e53d930c5f29439dd679c502
Alex Lementuev <a.lementuev@gmail.com>
2017-04-11 21:51:03 -0700

    Added more legacy files to clear

--- Project/Assets/LunarConsole/Editor/Autorun.cs
+++ Project/Assets/LunarConsole/Editor/Autorun.cs
@@ -36,10 +36,16 @@ namespace LunarConsoleEditorInternal
         private static readonly string[] kLegacyAssets =
         {
             "Assets/Plugins/Android/LunarConsole",
+
             "Assets/LunarConsole/Editor/Android/AndroidManifest.xml",
             "Assets/LunarConsole/Editor/Android/libs",
             "Assets/LunarConsole/Editor/Android/project.properties",
             "Assets/LunarConsole/Editor/Android/res",
+
+            "Assets/Plugins/LunarConsole/Editor/Android/AndroidManifest.xml",
+            "Assets/Plugins/LunarConsole/Editor/Android/libs",
+            "Assets/Plugins/LunarConsole/Editor/Android/project.properties",
+            "Assets/Plugins/LunarConsole/Editor/Android/res",
         };
 
         static Autorun()
