14f743569df1953eca00d7df2bb67f5e79b0de68
Alex Lementuev <a.lementuev@gmail.com>
2018-11-03 22:39:09 -0700

    Windows build system support

---
+++ Builder/platform.rb
@@ -0,0 +1,24 @@
+module Common
+  class Platform
+    def self.unity_export
+      return '/Applications/Unity-Export/Unity.app/Contents/MacOS/Unity' if is_mac_os
+      return 'C:\Program Files\Unity\Editor\Unity.exe'
+    end
+
+    def self.unity_publish
+      return '/Applications/Unity-Publish/Unity.app/Contents/MacOS/Unity' if is_mac_os
+      return 'C:\Program Files\Unity5\Editor\Unity.exe'
+    end
+
+    private
+    def self.is_mac_os
+      host_os = RbConfig::CONFIG['host_os']
+      case host_os
+        when /darwin|mac os/
+          return true
+        else
+          return false
+      end
+    end
+  end
+end
