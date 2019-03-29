6bf3cdcd08dcc3f32af506702d4e61609d3bccef
Alex Lementuev <a.lementuev@gmail.com>
2019-03-28 17:43:47 -0700

    Fixed warning text

--- Native/iOS/LunarConsole/LunarConsole/Full/Controllers/Actions/LUActionController.m
+++ Native/iOS/LunarConsole/LunarConsole/Full/Controllers/Actions/LUActionController.m
@@ -70,7 +70,7 @@ static const NSInteger kSectionCount = 2;
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     self.filterBar.delegate = self;
-    self.noActionsWarningLabel.text = @"You don't have any actions or variables yet";
+    self.noActionsWarningLabel.text = @"No actions or variables";
 }
 
 #pragma mark -
@@ -334,7 +334,7 @@ static const NSInteger kSectionCount = 2;
 - (void)updateNoActionWarningView
 {
     BOOL hasContent = [self actionCount] > 0 || [self variableCount] > 0;
-    [self setNoActionsWarningViewHidden:hasContent];
+	[self setNoActionsWarningViewHidden:hasContent];
 }
 
 - (void)setNoActionsWarningViewHidden:(BOOL)hidden
