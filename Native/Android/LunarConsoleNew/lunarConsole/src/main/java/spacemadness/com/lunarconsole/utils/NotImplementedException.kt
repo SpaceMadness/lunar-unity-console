946b899da314932a38693f7eff3c728a91b2b6fe
Alex Lementuev <a.lementuev@gmail.com>
2020-01-11 17:23:28 -0500

    Added NotImplementedException

---
+++ Native/Android/LunarConsoleNew/lunarConsole/src/main/java/spacemadness/com/lunarconsole/utils/NotImplementedException.kt
@@ -0,0 +1,30 @@
+//
+//  NotImplementedException.java
+//
+//  Lunar Unity Mobile Console
+//  https://github.com/SpaceMadness/lunar-unity-console
+//
+//  Copyright 2019 Alex Lementuev, SpaceMadness.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+package spacemadness.com.lunarconsole.utils
+
+/**
+ * The exception that is thrown when a requested method or operation is not implemented.
+ */
+class NotImplementedException : RuntimeException {
+    constructor()
+    constructor(message: String) : super(message)
+}
