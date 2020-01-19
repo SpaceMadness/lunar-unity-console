5682ae2c98c03cf95a6350aa27ecd66dbc2455be
Alex Lementuev <a.lementuev@gmail.com>
2020-01-19 18:53:36 -0500

    Refactoring
    Executor accepts Runnable instead of lambda

--- Native/Android/LunarConsoleNew/lunarConsole/src/main/java/spacemadness/com/lunarconsole/concurrent/Executor.kt
+++ Native/Android/LunarConsoleNew/lunarConsole/src/main/java/spacemadness/com/lunarconsole/concurrent/Executor.kt
@@ -3,5 +3,5 @@ package spacemadness.com.lunarconsole.concurrent
 import spacemadness.com.lunarconsole.core.TimeInterval
 
 interface Executor {
-    fun execute(callback: () -> Unit, delay: TimeInterval)
+    fun execute(task: Runnable, delay: TimeInterval = 0)
 }
