06a1c34e3f61a888be99599f7f99357536d4ecbd
Alex Lementuev <a.lementuev@gmail.com>
2020-02-11 14:38:49 -0500

    Log refactoring

--- Native/Android/LunarConsoleNew/lunarConsole/src/main/java/spacemadness/com/lunarconsole/log/Log.kt
+++ Native/Android/LunarConsoleNew/lunarConsole/src/main/java/spacemadness/com/lunarconsole/log/Log.kt
@@ -13,14 +13,25 @@ enum class LogLevel {
 object Log {
     private val logger = DependencyProvider.of<Logger>()
 
-    fun v(tag: LogTag, message: String) = log(LogLevel.Verbose, tag, message)
-    fun d(tag: LogTag, message: String) = log(LogLevel.Debug, tag, message)
-    fun i(tag: LogTag, message: String) = log(LogLevel.Info, tag, message)
-    fun w(tag: LogTag, message: String) = log(LogLevel.Warning, tag, message)
-    fun e(tag: LogTag, message: String) = log(LogLevel.Error, tag, message)
-    fun e(tag: LogTag, message: String, e: Throwable) = log(LogLevel.Error, tag, message, e)
+    fun v(tag: LogTag?, message: String) = log(LogLevel.Verbose, tag, message)
+    fun v(message: String) = v(null, message)
 
-    private fun log(level: LogLevel, tag: LogTag, message: String, throwable: Throwable? = null) {
+    fun d(tag: LogTag?, message: String) = log(LogLevel.Debug, tag, message)
+    fun d(message: String) = d(null, message)
+
+    fun i(tag: LogTag?, message: String) = log(LogLevel.Info, tag, message)
+    fun i(message: String) = i(null, message)
+
+    fun w(tag: LogTag?, message: String) = log(LogLevel.Warning, tag, message)
+    fun w(message: String) = w(null, message)
+
+    fun e(tag: LogTag?, message: String) = log(LogLevel.Error, tag, message)
+    fun e(message: String) = e(null, message)
+
+    fun e(tag: LogTag?, e: Throwable, message: String) = log(LogLevel.Error, tag, message, e)
+    fun e(e: Throwable, message: String) = e(null, message)
+
+    private fun log(level: LogLevel, tag: LogTag?, message: String?, throwable: Throwable? = null) {
         val buffer = StringBuilder()
 
         // thread name
@@ -29,8 +40,10 @@ object Log {
         buffer.append(']')
 
         // tag
-        buffer.append(' ')
-        buffer.append(tag.name)
+        if (tag != null) {
+            buffer.append(' ')
+            buffer.append(tag.name)
+        }
 
         // message
         buffer.append(' ')
