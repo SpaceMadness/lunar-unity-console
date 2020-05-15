a643519f8e2ae364491b3767424234f094e199ce
Alex Lementuev <a.lementuev@gmail.com>
2020-05-14 21:21:45 -0400

    Transform rich text string to NSAttributedString

--- Native/iOS/LunarConsole/LunarConsole/Common/Console/Plugin/LUConsolePlugin.m
+++ Native/iOS/LunarConsole/LunarConsole/Common/Console/Plugin/LUConsolePlugin.m
@@ -185,7 +185,17 @@ static NSString *const kScriptMessageTrackEvent = @"track_event";
 
 - (void)logMessage:(NSString *)message stackTrace:(NSString *)stackTrace type:(LUConsoleLogType)type
 {
-    LULogMessage *logMessage = [[LULogMessage alloc] initWithText:message attributedText:nil]; // TODO: attributed text
+    NSAttributedString *attributedText = nil;
+    if (_settings.richTextTags && LUStringContainsRichTextTags(message))
+    {
+        NSString *htmlString = LUStringRichTextToHtml(message);
+        attributedText = [[NSAttributedString alloc] initWithData:[htmlString dataUsingEncoding:NSUTF8StringEncoding]
+                                                          options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType }
+                                               documentAttributes:nil
+                                                            error:nil];
+        message = LUStringRemoveRichTextTags(message);
+    }
+    LULogMessage *logMessage = [[LULogMessage alloc] initWithText:message attributedText:attributedText];
     
     if ([self shouldDisplayErrorType:type] && _consoleWindow == nil) {
         [self showWarningWithMessage:message];
