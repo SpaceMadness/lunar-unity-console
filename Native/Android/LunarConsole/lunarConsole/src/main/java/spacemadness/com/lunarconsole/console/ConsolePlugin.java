12680def248ea67ff1c8a6e80cf5d2fb9bf5876e
Alex Lementuev <a.lementuev@gmail.com>
2020-05-26 22:38:43 -0400

    Updated copyrights

--- Native/Android/LunarConsole/lunarConsole/src/main/java/spacemadness/com/lunarconsole/console/ConsolePlugin.java
+++ Native/Android/LunarConsole/lunarConsole/src/main/java/spacemadness/com/lunarconsole/console/ConsolePlugin.java
@@ -1,10 +1,10 @@
 //
-//  ConsolePluginImpl.java
+//  ConsolePlugin.java
 //
 //  Lunar Unity Mobile Console
 //  https://github.com/SpaceMadness/lunar-unity-console
 //
-//  Copyright 2019 Alex Lementuev, SpaceMadness.
+//  Copyright 2020 Alex Lementuev, SpaceMadness.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
