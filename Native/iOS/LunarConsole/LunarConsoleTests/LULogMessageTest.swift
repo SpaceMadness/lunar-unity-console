11bf291a5e6980845530a49521d987a5e73fb3c3
Alex Lementuev <a.lementuev@gmail.com>
2020-07-28 22:21:50 -0400

    Cleanup test

--- Native/iOS/LunarConsole/LunarConsoleTests/LULogMessageTest.swift
+++ Native/iOS/LunarConsole/LunarConsoleTests/LULogMessageTest.swift
@@ -10,14 +10,6 @@ import XCTest
 
 class LULogMessageTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testNilString() throws {
         let expected = LULogMessage(text: nil, tags: nil);
         let actual = LULogMessage.fromRichText(nil);
