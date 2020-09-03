d085b4fdd61b7220e0d62b3fbe775c52152845b4
Alex Lementuev <a.lementuev@gmail.com>
2020-09-03 14:45:29 -0400

    Trigger native UI update when changing variable in code

--- Project/Assets/LunarConsole/Scripts/LunarConsole.cs
+++ Project/Assets/LunarConsole/Scripts/LunarConsole.cs
@@ -485,6 +485,12 @@ namespace LunarConsolePlugin
                         }
 
                         m_registry.Register(cvar);
+                        
+                        // trigger native UI-update
+                        cvar.AddDelegate(delegate(CVar v)
+                        {
+                            m_platform.OnVariableUpdated(m_registry, v);
+                        });
                     }
                 }
             }
